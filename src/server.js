a0f2036517d4109c463ad7a3fb11567399bc2ea1
Nethanel Reta <nethanel.n4@gmail.com>
2025-11-10 22:54:17 -0500

    added webhooks

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ const app = express()
 
 app.use(express.json());
 
+const WebHooks = require("./routes/webhooks")
+
 app.get('/', (req, res) => {
     res.json("Hello")
 })
+
+app.use('/webhook', WebHooks)
 
 app.listen(3000, () => {
     console.log("Server is running on 3000")
